3092c9041f9226392c5249095e4092684855991e
Alexandr <programalexandr@gmail.com>
2023-12-10 17:20:52 -0500

    some changes to style

--- components/GlobalStyles.js
+++ components/GlobalStyles.js
@@ -1,30 +1,30 @@
 import { StyleSheet, Platform, StatusBar } from "react-native-web";
 export default StyleSheet.create({
-    view: {
-        padding: 10,
-        backgroundColor: "rgba(0, 0, 0, 0.4)",
-        margin: 20,
-        marginTop: -8,
-        borderRadius: 5,
-    },
-    img: {
-        width: "95%",
-        aspectRatio: 1,
-        backgroundColor: "#071215",
-        borderRadius: 5,
-        borderWidth: 1,
-        borderColor: "transparent",
-        margin: 5,
-    },
-    imageView: {
-        flex: 1,
-        flexDirection: "column",
-        margin: 0,
-        justifyContent: "space-between",
-        alignContent: "center",
-    },
-    background: {
-        backgroundColor: "#1b2838",
-    },
-    list: {},
+  view: {
+    padding: 10,
+    backgroundColor: "rgba(0, 0, 0, 0.4)",
+    margin: 20,
+    marginTop: -5,
+    borderRadius: 5,
+  },
+  img: {
+    width: "95%",
+    aspectRatio: 1,
+    backgroundColor: "#071215",
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: "transparent",
+    margin: 5,
+  },
+  imageView: {
+    flex: 1,
+    flexDirection: "column",
+    margin: 0,
+    justifyContent: "space-between",
+    alignContent: "center",
+  },
+  background: {
+    backgroundColor: "#1b2838",
+  },
+  list: {},
 });
